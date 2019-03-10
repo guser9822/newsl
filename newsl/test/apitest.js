0e01bd4296976345335a9965156cd3960e47e0b1
guser9822 <riccardo.daniello13@gmail.com>
2019-03-10 23:32:55 +0100

    [#feature/newsletter_api] Added a test for getting users by email. Completed all tests.

--- newsl/test/apitest.js
+++ newsl/test/apitest.js
@@ -3,7 +3,7 @@ const https = require('https');
 
 serverModule.newslserver.start('test');
 
-//Test Hello api
+//Test for hello api
 var testHelloApi = function () {
     var options = {
         hostname: 'localhost',
@@ -41,6 +41,7 @@ var testHelloApi = function () {
 
 testHelloApi();
 
+//Test for get all users api
 var getAllUsers = function () {
 
     var options = {
@@ -85,8 +86,7 @@ var getAllUsers = function () {
 
 getAllUsers();
 
-
-//Test signup api
+//Test for signup api
 var signup = function (userInfo) {
 
     var postData = userInfo;
@@ -155,4 +155,68 @@ signup(JSON.stringify({
     cap: '44328'
 }));
 
+signup(JSON.stringify({
+    name: 'Gennar0',
+    surname: 'De Martino',
+    email: 'gen.tr5@live.com',
+    birthDate: '142/02/1957',
+    cap: '44328'
+}));
+
+signup(JSON.stringify({
+    name: 'Marta',
+    surname: 'Spanne',
+    email: 'sva@gmail.it',
+    birthDate: '01/12/1998',
+    cap: '443'
+}));
+
 getAllUsers();
+
+//Test for filter by email
+var getUserByEmail = function (email) {
+
+    var postData = email;
+    var options = {
+        hostname: 'localhost',
+        port: 8080,
+        path: '/newsletter/getbyemail',
+        method: 'POST',
+        rejectUnauthorized: false,
+
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(postData)
+        }
+    };
+
+    var req = https.request(options, (res) => {
+        console.log('START GET BY EMAIl API TEST : Find user');
+        console.log(' ');
+        console.log('statusCode:', res.statusCode);
+        console.log(' ');
+        console.log('headers:', res.headers);
+
+        res.on('data', (d) => {
+            process.stdout.write(d);
+        });
+
+        res.on('end', () => {
+            console.log(' ');
+            console.log('END  GET BY EMAIl API TEST');
+            console.log('-------------------------------------------------------');
+        })
+
+    });
+
+    req.on('error', (e) => {
+        console.error(e);
+    });
+
+    req.write(postData);
+    req.end();
+}
+
+getUserByEmail(JSON.stringify({ email: 'test92@nln.it' }));
+getUserByEmail(JSON.stringify({ email: 'binggame@hotmail.com' }));
+getUserByEmail(JSON.stringify({ email: 'rt1.com' }));
